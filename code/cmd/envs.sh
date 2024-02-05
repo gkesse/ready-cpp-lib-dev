e6b9af4f84a63d9e7b0326e0dc5c53587d7f573d
Gerard KESSE <tiakagerard@hotmail.com>
2024-02-05 01:30:11 +0100

    module env

--- code/cmd/envs.sh
+++ code/cmd/envs.sh
@@ -2,5 +2,5 @@
 export "LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH"
 #================================================
 export "GPROJECT_ENV=TEST"
-export "GPROJECT_DATA=/var/lib/readydev"
+export "GPROJECT_DATA=$HOME/.readydev"
 #================================================
