98c8e115918f5d0dde906e161ca702a076cc256d
Gerard KESSE <tiakagerard@hotmail.com>
2024-02-12 02:25:39 +0100

    module log

--- code/src/GConstant.h
+++ code/src/GConstant.h
@@ -17,7 +17,7 @@ const char* const DEBUG_LOG_DATE_FORMAT         = "%d/%m/%Y %H:%M:%S";
 //===============================================
 const char* const SOCKET_HOSTNAME               = "0.0.0.0";
 const int SOCKET_PORT                           = 9050;
-const int SOCKET_BACKLOG                        = 5;
+const int SOCKET_BACKLOG                        = 20;
 const int SOCKET_BUFFER_SIZE                    = 1024; //---------- 1Ko
 const int SOCKET_BUFFER_MAX                     = 1*1024*1024; //--- 1Mo
 //===============================================
